247ecd8e690093beaa9efcbd40adf071aadb3db4
GMX <554220959@qq.com>
2019-05-20 15:09:54 +0800

    login

--- src/main/java/com/example/ExamSys/security/MyUserDetailsService.java
+++ src/main/java/com/example/ExamSys/security/MyUserDetailsService.java
@@ -1,7 +1,10 @@
 package com.example.ExamSys.security;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -25,13 +28,14 @@ public class MyUserDetailsService implements UserDetailsService{
 	public UserDetails loadUserByUsername(String login){
 		// TODO Auto-generated method stub
 		log.debug("Authenticating {}", login);
+		System.out.println(login);
 		User user = userRepository.findOneByLogin(login).orElse(null);
 		if(user == null) {
 			throw new UsernameNotFoundException("user:" + login + " not found.");
 		}
-//		List<SimpleGrantedAuthority> authorities = (List<SimpleGrantedAuthority>) user.getAuthorities();
-////		List<GrantedAuthority> authorities = 
-////				authorityList.stream().map(e -> new SimpleGrantedAuthority(e.getName())).collect(Collectors.toList());
+		List<SimpleGrantedAuthority> authorities = (List<SimpleGrantedAuthority>) user.getAuthorities();
+//		List<GrantedAuthority> authorities = 
+//				authorityList.stream().map(e -> new SimpleGrantedAuthority(e.getName())).collect(Collectors.toList());
 //		UserDetails userDetails = new org.springframework.security.core.userdetails.User(
 //                user.getLogin(),user.getPassword(),authorities);
 //		System.out.println("login:" + user.getLogin() + "  password:" + user.getPassword());
