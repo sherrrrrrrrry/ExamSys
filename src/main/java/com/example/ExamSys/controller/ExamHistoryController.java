d6a786f25c2df6b684ae86b42db76d4647de0c82
GMX <554220959@qq.com>
2019-05-28 15:14:31 +0800

    雷达图

--- src/main/java/com/example/ExamSys/controller/ExamHistoryController.java
+++ src/main/java/com/example/ExamSys/controller/ExamHistoryController.java
@@ -1,10 +1,11 @@
 package com.example.ExamSys.controller;
 
-import com.example.ExamSys.dao.TranscriptRepository;
-import com.example.ExamSys.domain.Question;
-import com.example.ExamSys.domain.QuestionBank;
-import com.google.gson.JsonArray;
-import jdk.internal.org.objectweb.asm.util.TraceAnnotationVisitor;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -13,14 +14,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import com.example.ExamSys.dao.StudentRepository;
+import com.example.ExamSys.dao.TranscriptRepository;
+import com.example.ExamSys.domain.QuestionBank;
+import com.example.ExamSys.domain.Student;
+import com.example.ExamSys.domain.Transcript;
+import com.example.ExamSys.domain.User;
+import com.example.ExamSys.service.QuestionBankService;
+import com.example.ExamSys.service.UserService;
 
 @RestController
 @RequestMapping("Examhistory")
@@ -28,6 +32,15 @@ public class ExamHistoryController {
     private final Logger LOG = LoggerFactory.getLogger(AdminController.class);
     @Autowired
     private TranscriptRepository transcriptRepository;
+    
+    @Autowired
+    QuestionBankService questionBankService;
+    
+    @Autowired
+    UserService userService;
+    
+    @Autowired
+    StudentRepository studentRepository;
     /**
      * 获取学生历次考试成绩**/
     @RequestMapping(value = "/list", method = RequestMethod.POST, headers = "Accept=application/json")
@@ -53,6 +66,42 @@ public class ExamHistoryController {
             return ResponseEntity.ok().body(questionBankArray.toString());
         }
 
+    }
+    
+    /**
+     * 雷达图参数
+     */
+    @RequestMapping(value = "/getRedarMap", method = RequestMethod.GET)
+    public ResponseEntity<String> getRedarMap(@RequestParam(value = "name") String name, @RequestParam(value = "username") String username){
+    	
+    	 Optional<User> user = userService.findOneByLogin(username);
+         if (!user.isPresent()){
+             return ResponseEntity.badRequest().header("User","No such user!").body(null);
+         }
+         Student student = studentRepository.findStuByUser(user.get());
+         if (student== null){
+             return ResponseEntity.badRequest().header("Student","No such student!").body(null);
+         }
+         
+    	QuestionBank questionBank = questionBankService.findByName(name);
+        if (questionBank == null){
+            return ResponseEntity.badRequest().header("Exam","No such examination!").body(null);
+        }
+                
+    	ArrayList<Transcript> list = (ArrayList<Transcript>) transcriptRepository.findAllByQuestionBankIdAndStudentId(questionBank.getId(), student.getId());
+
+		JSONObject json = new JSONObject();
+    	
+    	for(Transcript transcript : list) {
+    		float score = transcript.getScore();
+    		try {
+        		json.put(transcript.getType(), (int)Math.ceil(score/transcript.getTotalScore() * 100));
+    		} catch(Exception e) {
+    			return ResponseEntity.badRequest().header("Json","Construct json data wrong!").body(null);
+    		}
+    	}
+    	
+    	return ResponseEntity.ok().body(json.toString());
     }
     
 }
